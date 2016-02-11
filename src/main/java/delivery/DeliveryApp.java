bdc550a86a67a5db9d4fa41efa5e3eb412b62a37
James Lowry <developer@jameslowry.co.uk>
2016-02-11 18:38:55 +0000

    Added main and test

---
+++ src/main/java/delivery/DeliveryApp.java
@@ -0,0 +1,12 @@
+package delivery;
+
+public class DeliveryApp {
+    public static void main(String[] args) {
+        DeliveryApp app = new DeliveryApp();
+        app.start();
+    }
+
+    private void start() {
+
+    }
+}
---
+++ src/test/java/tests/Tests.java
@@ -0,0 +1,7 @@
+package tests;
+
+/**
+ * Created by jay on 11/02/2016.
+ */
+public class Tests {
+}
